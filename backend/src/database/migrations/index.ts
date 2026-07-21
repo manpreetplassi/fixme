ab16134dc94e47739986b83974e4480f2950b4ad
manpreetplassi <manugirn07gmail.com>
2026-07-21 23:38:42 +0530

    feat: replace mobile nav with single scrollable tab bar

--- backend/src/database/migrations/index.ts
+++ backend/src/database/migrations/index.ts
@@ -2,5 +2,12 @@ import { InitialSchema1719600000000 } from './1719600000000-InitialSchema';
 import { TodayRoutineAndScreenCheckins1720100000000 } from './1720100000000-TodayRoutineAndScreenCheckins';
 import { LifestyleHealthTracking1720200000000 } from './1720200000000-LifestyleHealthTracking';
 import { RemoveLifestyleWaterTracking1720300000000 } from './1720300000000-RemoveLifestyleWaterTracking';
+import { UnifyRoutineMoneyAndHobbies1720400000000 } from './1720400000000-UnifyRoutineMoneyAndHobbies';
 
-export const migrations = [InitialSchema1719600000000, TodayRoutineAndScreenCheckins1720100000000, LifestyleHealthTracking1720200000000, RemoveLifestyleWaterTracking1720300000000];
+export const migrations = [
+  InitialSchema1719600000000,
+  TodayRoutineAndScreenCheckins1720100000000,
+  LifestyleHealthTracking1720200000000,
+  RemoveLifestyleWaterTracking1720300000000,
+  UnifyRoutineMoneyAndHobbies1720400000000,
+];
